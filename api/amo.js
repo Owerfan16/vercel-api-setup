0702b7aa6af4edf08ddeb3892f1ca74d84d90ec6
owerfan <zloukot16@gmail.com>
2025-09-21 21:14:57 +0900

    Convert to CommonJS format for better Vercel compatibility

--- api/amo.js
+++ api/amo.js
@@ -1,4 +1,4 @@
-export default async function handler(req, res) {
+module.exports = async function handler(req, res) {
   console.log(`API called: ${req.method} ${req.url}`);
 
   // Устанавливаем CORS заголовки
